acc32ba0229b8daaa77b027c6f828d4e76173e21
Sumitraj28 <rajsumit1228@gmail.com>
2026-03-27 19:25:26 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps Project Running 🚀");
+  res.send("Updated Version 🔥");
 });
 
 app.listen(3000, () => {
